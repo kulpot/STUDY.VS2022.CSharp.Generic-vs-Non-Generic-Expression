90700ef57c27fb59e1934af96f61ff2534bf704e
Kulpot <kulpotwahu@gmail.com>
2023-08-21 18:57:54 +0800

    Add project files.

---
+++ Generic vs Non Generic Expression/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq.Expressions;
+
+//ref link:https://www.youtube.com/watch?v=zgFTP0vaxSI&list=PLRwVmtr-pp06SlwcsqhreZ2byuozdnPlg&index=13
+//
+
+class MainClass
+{
+    static void Main()
+    {
+        Expression<Func<int, bool>> test = i => i > 5;
+        ConstantExpression c = Expression.Constant(5, typeof(int));
+        Console.WriteLine(c.Type);
+        Console.WriteLine(c.NodeType);
+        ParameterExpression p;
+        BinaryExpression b;
+    }
+}
